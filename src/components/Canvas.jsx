b8da353a532dcac19ca2a7527d1acb774968f7d5
Francis Martinez <f.martinez1993@icloud.com>
2025-11-09 23:49:29 -0500

    Commit test

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -5,7 +5,7 @@ import { useState } from "react";
 function Canvas() {
   const { qrText, customName, style } = useQRCode();
 
-  //temp variable
+  //temp variable/
   const [rotation, setRotation] = useState(0);
 
   return (
